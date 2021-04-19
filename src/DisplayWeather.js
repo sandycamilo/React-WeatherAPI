2b29644c9e266f2b6cfebdfbedbfe462612b9233
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2021-04-19 07:38:31 -0700

    fixed index.css so that it displays content in center of page

---
+++ src/DisplayWeather.js
@@ -0,0 +1,12 @@
+import './DisplayWeather.css';
+
+function DisplayWeather() {
+  return (
+    <div>
+      <h1>70</h1>
+      <p>Partly Cloudy</p>
+    </div>
+  )
+}
+
+export default DisplayWeather;
